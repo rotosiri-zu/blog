ab09e2f2b76c42f92bf633db3c9bba44ae4df144
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2019-12-07 16:06:28 +0900

    blog

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  def miniapp
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root  'tweets#index' 
+  resources :tweets do
+    resources :comments, only: [:create]
+    collection do
+      get :search
+    end
+  end
+
+  resources :users do
+    resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
+    collection do
+      get 'miniapp'
+    end
+  end  
+end
